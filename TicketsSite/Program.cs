930c1958b2c87863cb4253d4caffcfb397d887d8
Maksim Gornostaev <maksim.gornostaev197@gmail.com>
2024-03-11 23:07:16 +0300

    Fix code

--- TicketsSite/Program.cs
+++ TicketsSite/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 //DbContext configuration
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+builder.Services.AddDbContext<AppDbContext>(optionsBuilder => optionsBuilder.UseSqlServer("DefaultConnectionString"));
 
 var app = builder.Build();
 

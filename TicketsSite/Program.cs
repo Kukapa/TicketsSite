a5837d0b6ec9435a00fd51f6d80a64781f901685
Maksim Gornostaev <maksim.gornostaev197@gmail.com>
2024-03-12 01:04:11 +0300

    Update DbContext configuration

--- TicketsSite/Program.cs
+++ TicketsSite/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 //DbContext configuration
-builder.Services.AddDbContext<AppDbContext>(optionsBuilder => optionsBuilder.UseSqlServer("DefaultConnectionString"));
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 var app = builder.Build();
 

cabe6ccae14675b5637cf8c202b0bc6a825cf922
Maksim Gornostaev <maksim.gornostaev197@gmail.com>
2024-03-11 17:11:51 +0300

    Add relations

--- TicketsSite/Models/Movie.cs
+++ TicketsSite/Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Runtime.InteropServices.JavaScript;
 using TicketsSite.Data.Enums;
 
@@ -16,5 +17,15 @@ namespace TicketsSite.Models
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public MovieCategory MovieCategory { get; set; }
+
+        public List<Actor_Movie> Actors_Movies { get; set; }
+
+        public int CinemaId { get; set; }
+        [ForeignKey("CinemaId")]
+        public Cinema Cinema { get; set; }
+
+        public int ProducerId { get; set; }
+        [ForeignKey("ProducerId")]
+        public Producer Producer { get; set; }
     }
 }

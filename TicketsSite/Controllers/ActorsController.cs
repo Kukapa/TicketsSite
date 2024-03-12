239764a8cbeb3cb3510879cc6dcaa79a5883f2d3
Maksim Gornostaev <maksim.gornostaev197@gmail.com>
2024-03-12 18:53:21 +0300

    add code

--- TicketsSite/Controllers/ActorsController.cs
+++ TicketsSite/Controllers/ActorsController.cs
@@ -15,7 +15,7 @@ namespace TicketsSite.Controllers
         public IActionResult Index()
         {
             var data = _context.Actors.ToList();
-            return View();
+            return View(data);
         }
     }
 }

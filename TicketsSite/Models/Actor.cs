779e00e78b2401bdbb8f5cc47d9fade8c5be166b
Maksim Gornostaev <maksim.gornostaev197@gmail.com>
2024-03-14 19:35:58 +0300

    Add code

--- TicketsSite/Models/Actor.cs
+++ TicketsSite/Models/Actor.cs
@@ -8,10 +8,14 @@ namespace TicketsSite.Models
         public int Id { get; set; }
 
         [Display(Name = "Picture")]
+        [Required(ErrorMessage = "Picture is required")]
         public string PictureUrl { get; set; }
         [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "Full Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Full Name must be between 3 and 50 chars")]
         public string FullName { get; set; }
         [Display(Name = "Biography")]
+        [Required(ErrorMessage = "Biography is required")]
         public string Bio { get; set; }
 
         public List<Actor_Movie> Actors_Movies { get; set; }

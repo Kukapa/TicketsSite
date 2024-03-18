4996fede32069430d4dd46edb0d558dcd525aaa3
Maksim Gornostaev <maksim.gornostaev197@gmail.com>
2024-03-18 17:24:23 +0300

    Create Producer actions

---
+++ TicketsSite/Data/Services/IProducersService.cs
@@ -0,0 +1,10 @@
+﻿using TicketsSite.Data.Base;
+using TicketsSite.Models;
+
+namespace TicketsSite.Data.Services
+{
+    public interface IProducersService : IEntityBaseRepository<Producer>
+    {
+
+    }
+}
---
+++ TicketsSite/Data/Services/ProducersService.cs
@@ -0,0 +1,14 @@
+﻿using TicketsSite.Data.Base;
+using TicketsSite.Models;
+
+namespace TicketsSite.Data.Services
+{
+    public class ProducersService : EntityBaseRepository<Producer>, IProducersService
+    {
+        public ProducersService(AppDbContext context) : base(context)
+        {
+            
+        }
+
+    }
+}
